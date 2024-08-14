14775cbfe45407fe24edaabad326e498000add98
Hasanat Jahan <jahanhasanat@gmail.com>
2024-08-13 22:11:57 -0700

    first additions to new file

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Navbar from "./Navbar";
+import Home from "./Home";
 
 function App() {
+  // const title = "Welcome to the new blog";
+  //  react converts this to a string before it outputs it 
+  //  the only thing it cant directly output is objects 
+  // const likes = 500;
+  // const link = "http://www.google.com"
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar />
+      <div className="content">
+        <Home />
+      </div>
     </div>
   );
 }
 
+//  Export so we can use it for the files
 export default App;
