4bdbec720174b70ebe2b494001f0875cc46f9f03
Hasanat Jahan <jahanhasanat@gmail.com>
2024-08-14 23:04:02 -0700

    adding changes for blog list before making component

--- src/Home.js
+++ src/Home.js
@@ -1,8 +1,23 @@
+import { useState } from "react";
+
 function Home() {
+    const [blogs, setBlogs] = useState([
+        { title: "Miracle ways to cover foot fungus", body: "Syke there is none stinky", author: "deranged", id: 1 },
+        { title: "How to stop hating yourself", body: "Syke there is none depressy", author: "sad", id: 2 },
+        { title: "Actual helpful article please click", body: "Syke there is none dummy", author: "Noam Chomsky", id: 3 }
+    ]);
+
     return (
         <div className="home">
             <h2>Homepage</h2>
-        </div>
+            {blogs.map((blog) => (
+                <div className="blog-preview" key={blog.id}>
+                    <h2>{blog.title}</h2>
+                    <p1>Written by {blog.author}</p1>
+                </div>
+            ))
+            }
+        </div >
     );
 }
 
